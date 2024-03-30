bfed7f05a229f8b2b722499c8640720f27ff4753
Ssurafels <ssurafel4@gmail.com>
2024-03-30 11:53:35 +0300

    Add logic to focus on inputs

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import './contact.css';
-import SoicialIcons from './icons';
+import SocialIcons from './icons';
 
 const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [nameFocused, setNameFocused] = useState('');
+  const [emailFocused, setEmailFocused] = useState('');
+  const [messageFocused, setMessageFocused] = useState('');
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -19,13 +22,11 @@ const Contact = () => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
-    // Check if name, email, and message are not empty
     if (!name || !email || !message) {
       setErrorMessage('Please fill out all fields.');
       return;
     }
 
-    // Check email validity
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
       setErrorMessage('Please enter a valid email address.');
@@ -37,7 +38,6 @@ const Contact = () => {
     }
 
     try {
-      // Send form data to Formspree endpoint
       const response = await fetch('https://formspree.io/f/xvojodzg', {
         method: 'POST',
         headers: {
@@ -54,7 +54,6 @@ const Contact = () => {
         throw new Error('Failed to submit form.');
       }
 
-      // Reset form fields and error message on successful submission
       setName('');
       setEmail('');
       setMessage('');
@@ -67,23 +66,57 @@ const Contact = () => {
 
   return (
     <div id="footer">
-      <div className="m-3 d-flex flex-column flex-sm-row">
+      <div id="contact-form" className=" d-flex flex-column flex-sm-row background-svg">
         <div id="animate">
           <p className="slide-in" id="contact-detail">I'm always interested in hearing about new projects, so if you'd like to chat please get in touch.</p>
         </div>
         <form onSubmit={handleFormSubmit}>
-          <input type="text" name="name" placeholder="Full Name" maxLength="30" value={name} onChange={handleNameChange} required />
-          <input type="email" name="email" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <textarea name="message" placeholder="Hey, I'd like to connect..." cols="30" rows="10" maxLength="500" value={message} onChange={handleMessageChange} required />
-          <button type="submit" id="btn">Get in touch</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            maxLength="30"
+            value={name}
+            onChange={handleNameChange}
+            required
+            style={{ outline: 'none', borderColor: nameFocused ? 'green' : '#CFD9D4' }}
+            onFocus={() => setNameFocused(true)}
+            onBlur={() => setNameFocused(false)}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            style={{ outline: 'none', borderColor: emailFocused ? 'green' : '#CFD9D4' }}
+            onFocus={() => setEmailFocused(true)}
+            onBlur={() => setEmailFocused(false)}
+          />
+          <textarea
+            name="message"
+            placeholder="Hey, I'd like to connect..."
+            cols="30"
+            rows="10"
+            maxLength="500"
+            value={message}
+            onChange={handleMessageChange}
+            required
+            style={{ outline: 'none', borderColor: messageFocused ? 'green' : '#CFD9D4' }}
+            onFocus={() => setMessageFocused(true)}
+            onBlur={() => setMessageFocused(false)}
+          />
+          <button className="get-in-touch-button" type="submit" id="btn">Get in touch</button>
           {errorMessage && <p className="error-message text-danger">{errorMessage}</p>}
         </form>
       </div>
 
       <div className="d-flex-column text-center">
         <p id="email"><a className="text-decoration-none text-reset" href="mailto:ssurafel4@gmail.com">ssurafel4@gmail.com</a></p>
-        <SoicialIcons />
+        <SocialIcons />
         <hr />
+
       </div>
     </div>
   );
