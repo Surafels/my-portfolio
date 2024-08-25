0c90a0daa62655bd9d77bfeb290c204f5f9d7ca7
ssurafel4@gmail.com <ssurafel4@gmail.com>
2024-08-26 01:28:30 +0300

    Add Google Analytics Tracker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import GoogleAnalyticsTracker from './components/GoogleAnalyticsTracker';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
+      <GoogleAnalyticsTracker />
       <App />
     </React.StrictMode>
     ,
